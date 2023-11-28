11196c3054547e964829e7e94854d4ff327af3d2
LukaszBartosikPep <139876454+LukaszBartosikPep@users.noreply.github.com>
2023-11-28 10:49:00 +0100

    Solution more with sliding window

--- Longest Substring Without Repeating Characters.java
+++ Longest Substring Without Repeating Characters.java
@@ -3,27 +3,45 @@ import java.util.Arrays;
 
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-         for(int i=0; i<strList.size()-1;i++){
+       int result=0;
+
+            String[] strSplit=s.split("");
+        ArrayList<String> strList=new ArrayList<String>(
+            Arrays.asList(strSplit)
+        );
+        List<String> lengthList=new ArrayList<>();
+
+        List<Integer> ListOfLenghts=new ArrayList<>();
+
+        for(int i=0; i<strList.size()-1;i++){
             
-            lengthList.add(strList.get(i));
+            lengthList.add(strList.get(i));   //this should be the window
             if(lengthList.contains(strList.get(i+1))){
+                int index=lengthList.size();
+                lengthList.remove(0);
+                
+                ListOfLenghts.add(index);
             
-            firstLength=lengthList.size();
-            ListOfLengths.add(firstLength);
-            // l=lengthList.size();
-            lengthList.clear();
-            // break;
+        
+            
+            
+           
              }
             }
-        Collections.sort(ListOfLengths);
 
-       for (int value : ListOfLengths) {
-            System.out.println(value);
+
+
+
+         Collections.sort(ListOfLenghts);
+
+
+        for(int value:ListOfLenghts){
+
+            System.out.print(value);
             result=value;
+        }
 
-            }
-
-        return test;
+        return result;
         
     }
 }
